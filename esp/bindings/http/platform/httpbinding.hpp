410277c9ab49fef495a5a09ad34854e9f6b229a3
Rodrigo Pastrana <Rodrigo.Pastrana@lexisnexis.com>
2013-10-07 17:09:49 -0400

    HPCC-10175 - Remove RoxieTest page options
    Signed-off-by: Rodrigo Pastrana <Rodrigo.Pastrana@lexisnexis.com>

--- esp/bindings/http/platform/httpbinding.hpp
+++ esp/bindings/http/platform/httpbinding.hpp
@@ -147,7 +147,6 @@ protected:
     bool                    m_includeSoapTest;
     StringBuffer            m_challenge_realm;
     StringAttr              m_defaultSvcVersion;
-    bool                    m_roxieOption;
 
 public:
     EspHttpBinding(IPropertyTree* cfg, const char *bindname=NULL, const char *procname=NULL);
